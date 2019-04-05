d1133fcd8688555a3f242b3d06532d225c0e6dfe
Prayudi Tirta Bayu <prayuditb@gmail.com>
2019-04-05 23:29:17 +0700

    prepare initial dependencies

---
+++ src/config/index.ts
@@ -0,0 +1,22 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+    use_env_variable: 'DATABASE_URL'
+  },
+  test: {
+    dialect: "sqlite",
+    storage: ":memory:"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'mysql',
+    use_env_variable: 'DATABASE_URL'
+  }
+};
